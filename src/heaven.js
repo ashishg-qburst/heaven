906aa4b1b89ab45ac11f3d0ba909bce8f265a7ed
ash <ashishg@qburst.com>
2017-08-04 18:28:01 +0530

    Stars in Heaven

--- src/heaven.js
+++ src/heaven.js
@@ -1,16 +1,22 @@
-let props;
-
 function applyConfig(config) {
   let props = config;
-  props.starCount = config.starCount || 3;
+  props.count = config.count || 3;
   return props;
 }
 
 function init(element, config = {}) {
-  typeof element === 'string' ? element = document.querySelector(element) : element = element;
-  props = applyConfig(config);
+  typeof element === "string"
+    ? (element = document.querySelector(element))
+    : (element = element);
+  console.log(element);
+  element.heaven = {};
+  element.heaven.props = applyConfig(config);
+  element.heaven.showProps = showProps;
+  return element;
 }
 
-let showProps = () => { console.log(props); }
+function showProps() {
+  console.log(this.props);
+}
 
 export { init, showProps };
